b8782634fd6056df01e6c30230abd01d4597f14b
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-08-03 16:14:20 +0900

    [add] Make it support the Position3d class extending Eigen

---
+++ include/type/position3d.hpp
@@ -0,0 +1,36 @@
+//
+//  position3d.hpp
+//  RayTracer
+//
+//  Created by Shuto on 2016/08/03.
+//
+//
+
+#ifndef position3d_hpp
+#define position3d_hpp
+
+#include <Eigen/Core>
+
+namespace shkm {
+    class Position3d;
+}
+
+class shkm::Position3d : public Eigen::Vector3d
+{
+public:
+    explicit Position3d()
+    : shkm::Position3d(0, 0, 0)
+    {
+        
+    }
+    
+    ~Position3d() = default;
+    
+    explicit Position3d(const double x, const double y, const double z)
+    : Eigen::Vector3d(x, y, z)
+    {
+        
+    }
+};
+
+#endif /* position3d_hpp */
