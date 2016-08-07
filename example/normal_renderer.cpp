909fc4b4e30ea5f8d661e00beedc5eef690afae9
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-08-07 18:29:46 +0900

    [add] Make it support normal rendering

---
+++ example/normal_renderer.cpp
@@ -0,0 +1,43 @@
+//
+//  normal_renderer.cpp
+//  RayTracer
+//
+//  Created by Shuto Shikama on 2016/08/02.
+//
+//
+
+#include <iostream>
+#include <image/JpegImage.hpp>
+#include <image/BitmapImage.hpp>
+#include "algorithm/normal_renderer.hpp"
+#include "scene/world.hpp"
+#include "scene/bullet_world.hpp"
+
+int main(int argc, char** argv)
+{
+    constexpr unsigned int kWidth = 640;
+    constexpr unsigned int kHeight = 480;
+    shkm::BulletWorld world;
+    shkm::World::setTestScene(&world);
+    
+    fj::JpegImage image;
+    image.initialize(kWidth, kHeight);
+    
+    shkm::NormalRenderer algorithm;
+    
+    fj::NormalizedColor color;
+    for (int i = 0; i < kWidth; i++){
+        for (int j = 0; j < kHeight; j++)
+        {
+            const shkm::Position3d kFrom(0,0,-150);
+            const shkm::Position3d kTo(i-320,j-240,0);
+            const fj::NormalizedColor kColor = algorithm.render(kFrom, kTo, std::cref(world));
+
+            image.setAt(i, j, kColor);
+        }
+    }
+    
+    image.saveToFile( std::string("normal_renderer.jpeg") );
+    
+    return EXIT_SUCCESS;
+}
