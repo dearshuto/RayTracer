e545cc0387dbbbe362164864a3302e375b76d05f
Shuto Shikama <@gmail.com>
2022-02-13 01:50:18 +0900

    デフォルトで描画するシーンを Cornell Box に変更
    光源の色が適当だけど一旦マージ。

--- sjrt/src/scene/primitive.rs
+++ sjrt/src/scene/primitive.rs
@@ -1,6 +1,7 @@
 pub enum Primitive {
     Sphere(SphereData),
     Box(BoxData),
+    TriMesh(TriMeshData),
 }
 
 pub struct SphereData {
@@ -10,4 +11,10 @@ pub struct SphereData {
 pub struct BoxData {
     pub width: f32,
     pub height: f32,
+    pub depth: f32,
+}
+
+pub struct TriMeshData {
+    pub positions: Vec<f32>,
+    pub indices: Vec<u32>,
 }
