632bac5c4ccdeccbcb60c73d3b3dc9613c014295
Shuto Shikama <@gmail.com>
2025-02-09 16:50:03 +0900

    C# の with 構文と同様の機能を生成するマクロを追加

---
+++ sjrt-macro/src/lib.rs
@@ -0,0 +1,62 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{ext::IdentExt, parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Immutable)]
+pub fn immutable_device(input: TokenStream) -> TokenStream {
+    // 構造体じゃなければエラー
+    let input = &parse_macro_input!(input as DeriveInput);
+    let syn::Data::Struct(struct_data) = &input.data else {
+        // TODO
+        panic!();
+    };
+
+    let mut with_fields = Vec::default();
+    for (target_index, target_field) in struct_data.fields.iter().enumerate() {
+        let mut init_fields = Vec::default();
+
+        for (with_index, with) in struct_data.fields.iter().enumerate() {
+            if target_index == with_index {
+                continue;
+            }
+
+            // Self {
+            //    value: self.value // ⇦ ここを生成
+            // }
+            let name_info = with.ident.as_ref().unwrap();
+            init_fields.push(quote! {
+            #name_info: self.#name_info,
+                });
+        }
+
+        // 関数本体の生成
+        // fieldの型情報
+        let ty = &target_field.ty;
+        let name_info = target_field.ident.as_ref().unwrap();
+
+        // with_name のようにフィールド名に with_ をつけた名称を生成
+        let generated_method_name: proc_macro2::TokenStream =
+            format!("with_{}", name_info.unraw().to_string())
+                .parse()
+                .unwrap();
+        with_fields.push(quote! {
+            pub fn #generated_method_name(self, value: #ty) -> Self {
+                Self {
+                    #name_info: value,
+                    #(#init_fields)*
+                }
+            }
+        });
+    }
+
+    // 構造体名
+    let struct_name = &input.ident;
+
+    let expanded = quote! {
+    impl #struct_name  {
+        #(#with_fields)*
+    }
+     };
+
+    TokenStream::from(expanded)
+}
