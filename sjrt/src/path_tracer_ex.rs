a90a3486a4041601bd723f167e6c8efcd69a6780
Shuto Shikama <shuto.shikama@gmail.com>
2025-02-11 00:03:30 +0900

    fixup! [WIP] 新設計にパストレーサーを移植

--- sjrt/src/path_tracer_ex.rs
+++ sjrt/src/path_tracer_ex.rs
@@ -1,4 +1,4 @@
-use crate::{HitAction, IRayTracingPipeline, RayParams, Vector3f};
+use crate::{EntryParams, HitAction, IRayTracingPipeline, RayParams, Vector3f};
 
 pub trait IHitParams {
     fn normal(&self) -> Vector3f;
@@ -29,8 +29,9 @@ where
     current_sampling: u32,
     values: Vec<(Vector3f /*albedo*/, Vector3f /*emission*/)>,
     random_engine: T,
-    from: Vector3f,
-    to: Option<Vector3f>,
+
+    latest_hit_position: Vector3f,
+    latest_hit_normal: Vector3f,
 }
 
 pub struct PathTracerEx<T, TKernel>
@@ -47,39 +48,22 @@ where
     TKernel: IKernel,
 {
     type PayloadType = Payload<TKernel::RondomEngine>;
-
-    // TODO: rapier3d に限定しないよう抽象化する
     type HitParams = T;
 
-    fn entry(&self) -> impl Iterator<Item = Self::PayloadType> {
-        [Payload {
+    fn entry(&self, _entry_params: &EntryParams) -> Self::PayloadType {
+        Payload {
             current_depth: 0,
             current_sampling: 0,
             values: Vec::default(),
             random_engine: self.kernel.random_engine(),
-            from: Vector3f::zero(),
-            to: None,
-        }]
-        .into_iter()
-    }
-
-    fn trace(&self, payload: Self::PayloadType) -> crate::TraceAction<Self::PayloadType> {
-        // 反射回数が規定回数を超えていたら終了
-        if self.depth < payload.current_depth {
-            return crate::TraceAction::Finish(payload);
+            latest_hit_normal: Vector3f::zero(),
+            latest_hit_position: Vector3f::zero(),
         }
-
-        let ray_params = RayParams {
-            from: Vector3f::zero(),
-            to: Vector3f::zero(),
-            payload,
-        };
-        crate::TraceAction::Next(ray_params)
     }
 
     fn react_closest_hit(
         &self,
-        mut payload: Self::PayloadType,
+        payload: Self::PayloadType,
         hit_params: &Self::HitParams,
     ) -> crate::HitAction<
         Self::PayloadType,
@@ -89,23 +73,23 @@ where
             return HitAction::RayGenerate([].into_iter());
         }
 
-        let ratio_x = payload.random_engine.generate();
-        let ratio_y = payload.random_engine.generate();
-        let ratio_z = payload.random_engine.generate();
+        // 反射回数である深度を増やしつつヒット情報を保持してレイの生成に進む
         let normal = hit_params.normal();
-        let new_to = 500.0
-            * Vector3f::new(normal.x * ratio_x, normal.y * ratio_y, normal.z * ratio_z).normalize();
-
         let position = hit_params.position();
-
-        // 反射回数をひとつ増やして再びレイの生成判定へ
         let new_depth = payload.current_depth + 1;
-        return HitAction::Payload(
+
+        let albedo = hit_params.albedo();
+        let emission = hit_params.emission();
+        let mut values = payload.values.clone();
+        values.push((albedo, emission));
+
+        HitAction::Payload(
             payload
                 .with_current_depth(new_depth)
-                .with_from(position)
-                .with_to(Some(new_to)),
-        );
+                .with_latest_hit_position(position)
+                .with_latest_hit_normal(normal)
+                .with_values(values),
+        )
     }
 
     fn react_hit_miss(&self, payload: Self::PayloadType) -> Self::PayloadType {
@@ -124,5 +108,33 @@ where
 
         // 何回か反射してからミスしたら色は更新しない
         payload.with_current_depth(next_depth)
+    }
+
+    fn trace(
+        &self,
+        ray_params: RayParams<Self::PayloadType>,
+    ) -> crate::TraceAction<Self::PayloadType> {
+        let mut payload = ray_params.payload;
+
+        // 反射回数が規定回数を超えていたら終了
+        if self.depth < payload.current_depth {
+            return crate::TraceAction::Finish(payload);
+        }
+
+        // 最初にヒットしたポイントの情報から次にレイを飛ばす方向を決める
+        // とりあえず適当に乱数を生成して法線の向きに飛ばす
+        let normal = payload.latest_hit_normal;
+        let ratio_x = payload.random_engine.generate();
+        let ratio_y = payload.random_engine.generate();
+        let ratio_z = payload.random_engine.generate();
+        let new_to = 500.0
+            * Vector3f::new(normal.x * ratio_x, normal.y * ratio_y, normal.z * ratio_z).normalize();
+
+        let ray_params = RayParams {
+            from: payload.latest_hit_position,
+            to: new_to,
+            payload,
+        };
+        crate::TraceAction::Next(ray_params)
     }
 }
