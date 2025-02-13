59bb3bf34c28ae14de078ed7caca50ac745999b1
Shuto Shikama <@gmail.com>
2025-02-13 13:02:10 +0900

    廃止予定の実装を削除

--- sjrt/examples/hello_ray_tracer.rs
+++ sjrt/examples/hello_ray_tracer.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use image::GenericImage;
 use sjrt::scene::{primitive::SphereData, Material};
 
 #[derive(Parser, Debug)]
@@ -8,8 +9,26 @@ struct Args {
     sampling_count: u16,
 }
 
+struct Image(image::DynamicImage);
+
+impl sjrt::IColorBuffer for &mut Image {
+    fn write(&mut self, x: u32, y: u32, color: sjrt::Color) {
+        let data = match color {
+            sjrt::Color::R8G8B8A8_Uint(data) => data,
+            sjrt::Color::R32G32B32A32_Unorm(data) => [
+                (data[0] * 255.0).clamp(0.0, 255.0) as u8,
+                (data[1] * 255.0).clamp(0.0, 255.0) as u8,
+                (data[2] * 255.0).clamp(0.0, 255.0) as u8,
+                (data[3] * 255.0).clamp(0.0, 255.0) as u8,
+            ],
+        };
+
+        self.0.put_pixel(x, y, image::Rgba::from(data));
+    }
+}
+
 pub fn main() {
-    let args = Args::parse();
+    let _args = Args::parse();
     let scene_data = sjrt::scene::Scene {
         sky: sjrt::scene::Sky {
             lower_color: nalgebra::Vector3::zeros(),
@@ -35,10 +54,10 @@ pub fn main() {
         ],
     };
     let scene = sjrt::util::RapierScene::new_from_scene(&scene_data);
-    let renderer = sjrt::PathTracer::new(args.sampling_count, 4, false);
-    let mut buffer = sjrt::util::ImageBuffer::new(480, 480);
-    let system = sjrt::System::new();
-    system.execute(&scene, &mut buffer, &renderer);
+    let renderer = sjrt::PathTracerEx::default();
+    let mut buffer = Image(image::DynamicImage::new_rgba8(640, 480));
 
-    buffer.save("example.png");
+    sjrt::Executor::default().execute(&mut buffer, scene, renderer);
+
+    buffer.0.save("example.png").unwrap();
 }
