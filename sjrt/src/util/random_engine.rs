055d5dee1c25e9428f130100d988ee4894031d72
Shuto Shikama <@gmail.com>
2025-02-15 22:21:21 +0900

    [WIP] 乱数生成の Sync + Send 制約を撤廃

--- sjrt/src/util/random_engine.rs
+++ sjrt/src/util/random_engine.rs
@@ -1,20 +1,33 @@
-use rand::Rng;
+use std::sync::{Arc, Mutex};
 
 use crate::traits::IRandomEngine;
 
-pub struct RandomEngine {
-    rng: rand::rngs::ThreadRng,
-}
+#[derive(Clone)]
+pub struct RandomEngine;
 
 impl RandomEngine {
     pub fn new() -> Self {
-        let rng = rand::thread_rng();
-        Self { rng }
+        Self {}
     }
 }
 
 impl IRandomEngine<f32> for RandomEngine {
     fn generate_range(&mut self, range: std::ops::Range<f32>) -> f32 {
-        self.rng.gen_range(range)
+        // [0, 1)
+        let random = rand::random::<f32>();
+        let length = range.end - range.start;
+        length * random - range.end
+    }
+}
+
+impl<T> IRandomEngine<f32> for Arc<Mutex<T>>
+where
+    T: IRandomEngine<f32>,
+{
+    fn generate_range(&mut self, range: std::ops::Range<f32>) -> f32 {
+        // [0, 1)
+        let random = rand::random::<f32>();
+        let length = range.end - range.start;
+        length * random - range.end
     }
 }
