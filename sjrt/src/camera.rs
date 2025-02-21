a1bc8da8f2beb42154358320f900caa4c276c588
Shuto Shikama <@gmail.com>
2025-02-21 23:05:09 +0900

    nalgebra への依存を削減

--- sjrt/src/camera.rs
+++ sjrt/src/camera.rs
@@ -1,36 +1,65 @@
-use std::ops::Range;
+use std::ops::{Add, Mul, Range, Sub};
 
-pub struct Builder {
+use crate::{traits::INormalized, IConstract, INorm, IOuterProduct};
+
+pub struct Builder<TPoint> {
     field_of_view: f32,
-    position: nalgebra::Vector3<f32>,
-    look_at: nalgebra::Vector3<f32>,
+    position: TPoint,
+    look_at: TPoint,
 }
 
-impl Builder {
+impl<TPoint> Builder<TPoint>
+where
+    TPoint: num::Zero + IConstract<f32>,
+{
     fn new() -> Self {
         Self {
             field_of_view: std::f32::consts::PI / 4.0,
-            position: nalgebra::Vector3::zeros(),
-            look_at: nalgebra::Vector3::new(0.0, 0.0, -1.0),
+            position: TPoint::zero(),
+            look_at: TPoint::new(0.0, 0.0, -1.0),
         }
     }
+}
 
-    pub fn build(self) -> Camera {
+impl<TPoint> Builder<TPoint>
+where
+    TPoint: Copy,
+{
+    pub fn with_position(mut self, position: &TPoint) -> Self {
+        self.position = *position;
+        self
+    }
+
+    pub fn with_look_at(mut self, look_at: &TPoint) -> Self {
+        self.look_at = *look_at;
+        self
+    }
+}
+
+impl<TPoint> Builder<TPoint>
+where
+    TPoint: Copy
+        + num::Zero
+        + IConstract<f32>
+        + Mul<f32, Output = TPoint>
+        + Sub<Output = TPoint>
+        + INorm
+        + INormalized
+        + IOuterProduct,
+{
+    pub fn build(self) -> Camera<TPoint> {
         let direction = self.look_at - self.position;
         let distance_to_look_at = direction.norm();
-        let direction = nalgebra::Vector3::new(direction.x, direction.y, direction.z);
-        let right_vector = direction
-            .cross(&nalgebra::Vector3::new(0.0, 1.0, 0.0))
-            .normalize();
+        let right_vector = direction.cross(&TPoint::new(0.0, 1.0, 0.0)).normalized();
 
-        let up_vector = right_vector.cross(&direction).normalize();
+        let up_vector = right_vector.cross(&direction).normalized();
         let up_distance = distance_to_look_at * (self.field_of_view.tan());
-        let top_center = direction + up_distance * up_vector;
+        let top_center = direction + up_vector * up_distance;
 
         Camera {
-            center_top: nalgebra::Vector3::new(top_center.x, top_center.y, top_center.z),
-            right_vector: nalgebra::Vector3::new(right_vector.x, right_vector.y, right_vector.z),
-            up_vector: nalgebra::Vector3::new(up_vector.x, up_vector.y, up_vector.z),
+            center_top: top_center,
+            right_vector,
+            up_vector,
             half_height: up_distance,
             position: self.position,
         }
@@ -40,36 +69,35 @@ impl Builder {
         self.field_of_view = fov;
         self
     }
-
-    pub fn with_position(mut self, position: &nalgebra::Vector3<f32>) -> Self {
-        self.position = *position;
-        self
-    }
-
-    pub fn with_look_at(mut self, look_at: &nalgebra::Vector3<f32>) -> Self {
-        self.look_at = *look_at;
-        self
-    }
 }
 
-pub struct Camera {
-    center_top: nalgebra::Vector3<f32>,
-    right_vector: nalgebra::Vector3<f32>,
-    up_vector: nalgebra::Vector3<f32>,
+pub struct Camera<TVector> {
+    center_top: TVector,
+    right_vector: TVector,
+    up_vector: TVector,
     half_height: f32,
-    position: nalgebra::Vector3<f32>,
+    position: TVector,
 }
 
-impl Camera {
-    pub fn builder() -> Builder {
+impl Camera<nalgebra::Vector3<f32>> {
+    pub fn builder() -> Builder<nalgebra::Vector3<f32>> {
         Builder::new()
     }
+}
 
-    pub fn position(&self) -> &nalgebra::Vector3<f32> {
+impl<TVector> Camera<TVector>
+where
+    TVector: Copy
+        + Add<TVector, Output = TVector>
+        + Sub<Output = TVector>
+        + Mul<f32, Output = TVector>
+        + INormalized,
+{
+    pub fn position(&self) -> &TVector {
         &self.position
     }
 
-    pub fn calculate_ray_direction(&self) -> Vec<RayInfo> {
+    pub fn calculate_ray_direction(&self) -> Vec<RayInfo<TVector>> {
         self.calculate_ray_direction_range(640, 480, 0..640, 0..480)
     }
 
@@ -79,7 +107,7 @@ impl Camera {
         height: u32,
         width_range: Range<u32>,
         height_range: Range<u32>,
-    ) -> Vec<RayInfo> {
+    ) -> Vec<RayInfo<TVector>> {
         assert!(width_range.end <= width);
         assert!(height_range.end <= height);
 
@@ -89,19 +117,19 @@ impl Camera {
         let height = height as f32;
         let aspect = width / height;
         let half_width = self.half_height * aspect;
-        let top_left = self.center_top - half_width * self.right_vector;
+        let top_left = self.center_top - self.right_vector * half_width;
 
         // 解像度で分割してレイを飛ばすストライドを算出
         // 原点は左上
-        let stride_x = (2.0 * half_width / width) * self.right_vector;
-        let stride_y = (2.0 * self.half_height / height) * (-1.0 * self.up_vector);
+        let stride_x = self.right_vector * (2.0 * half_width / width);
+        let stride_y = (self.up_vector * (-1.0)) * (2.0 * self.half_height / height);
 
         for y in height_range {
             for x in width_range.clone() {
                 // レイを飛ばす方法を画面左上からのオフセットとして算出
-                let offset = (x as f32) * stride_x + (y as f32) * stride_y;
+                let offset = stride_x * (x as f32) + stride_y * (y as f32);
                 let to = top_left + offset;
-                let directioin = (to - self.position).normalize();
+                let directioin = (to - self.position).normalized();
 
                 let ray_info = RayInfo {
                     x,
@@ -116,10 +144,10 @@ impl Camera {
     }
 }
 
-pub struct RayInfo {
+pub struct RayInfo<T> {
     pub x: u32,
     pub y: u32,
-    pub directions: Vec<nalgebra::Vector3<f32>>,
+    pub directions: Vec<T>,
 }
 
 #[cfg(test)]
